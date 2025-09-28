53028b97adf2d3a13f72efe9ea4a9889b3cb83a4
Brave12-3 <163505747+Brave12-3@users.noreply.github.com>
2025-09-28 12:15:55 +0300

    Update todo.js

--- todo.js
+++ todo.js
@@ -1,10 +1,9 @@
 
-let name= prompt("Enter your name");
+/*let name= prompt("Enter your name");
 alert("Aselamualykum"+" "+ name);
 
 console.log("welcome" +" "+ name);
-
-//task object
+*/
 let tasks = {
    "task1": { name: "clean house", completed: false, id: 1 },
   "task2": { name: "prepare meal", completed: false, id: 2 },
@@ -19,32 +18,4 @@ let tasks = {
 
 
 
- let task = [{name: "clean house",completed:false,id:1},
-   {name: "prepare meal",completed:false,id: 2},
-   {name: "reading quran",completed:true,id: 3},
-   {name: "finish project",completed:true,id: 4},
-   {name: "Exercise",completed:false,id: 5},
-   {name: "Azkar",completed:false,id: 6},
-]
-
- for (let i = 0; i < tasks.length; i++) {
-  let t = tasks[i];
-  if (t.completed) {
-    console.log("Task " + t.id + ": " + t.name + " is completed!");
-  } else {
-    console.log(" Task " + t.id + ": " + t.name + " is not done yet.");
-  }
-}
-function showTasks(tasksArray) {
-  for (let i = 0; i < tasksArray.length; i++) {
-    let t = tasksArray[i];
-    if (t.completed) {
-      console.log("Task " + t.id + ": " + t.name + " is completed!");
-    } else {
-      console.log("Task " + t.id + ": " + t.name + " is not done yet.");
-    }
-  }
-}
-
-
-showTasks(tasks);
+ 
